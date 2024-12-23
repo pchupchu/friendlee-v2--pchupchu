979557c6b73859e1b124fab9238d6c48f9550eda
Marina <pchupchu@yandex.ru>
2024-12-23 18:14:48 +0300

    Add mobile hero section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
+import { createCanvas } from './create-canvas.js';
 import { createMobileMenuToggle } from './create-mobile-menu-toggle.js';
 
 // Вызываем функции после загрузки документа
 document.addEventListener('DOMContentLoaded', () => {
   // createCanvas();
   createMobileMenuToggle();
+  createCanvas();
 });
