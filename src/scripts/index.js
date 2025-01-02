decd1a40e3f1c64207374564dbbd0c471f3d605b
Marina <pchupchu@yandex.ru>
2025-01-02 14:28:37 +0300

    Add services animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,9 +5,50 @@ import { initHeaderScroll } from './init-header-scroll.js';
 
 // Вызываем функции после загрузки документа
 document.addEventListener('DOMContentLoaded', () => {
-  // createCanvas();
   createMobileMenuToggle();
   createCanvas();
   createFooterCanvas();
   initHeaderScroll();
 });
+
+const services = document.querySelector('.services');
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: services,
+    start: 'top center',
+    end: 'bottom center',
+    scrub: 2,
+    ease: 'none',
+  },
+  defaults: {
+    ease: 'none',
+  },
+});
+
+tl.from('.services__secondary-list', {
+  paddingTop: '350px',
+})
+  .from(
+    '.services__list-item--second-child',
+    {
+      y: '350px',
+    },
+    '<'
+  )
+  .from(
+    '.services__list-item--third-child',
+    {
+      y: '100px',
+    },
+    '<'
+  );
+
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
